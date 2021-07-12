04deec4f2ec1fd282339748da841b841cd65e9ed
maicol <maicol.matteoli@gmail.com>
2021-07-12 12:03:14 +0200

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header_wrapper">
+      header
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Header"
+}
+</script>
+
+<style lang="scss" scoped>
+    .header_wrapper{
+        height: 70px;
+        background: #2c3e50;
+    }
+</style>
