efefea760a0dfda4df94de03087822b40cea4edf
maicol <maicol.matteoli@gmail.com>
2021-07-12 18:34:15 +0200

    add : test logs

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header_wrapper container-fluid d-flex justify-content-center align-items-center">
-      <input type="text" placeholder="inserisci un titolo"  v-model="francesco" class="mx-2">
+      <input type="text" placeholder="inserisci un titolo"  v-model="ricerca" class="mx-2">
       <button type="button" class="btn btn-secondary" @click="getFilms">Cerca</button>
   </div>
 </template>
@@ -15,20 +15,33 @@ export default {
     },
     data() {
         return {
-            francesco:"",
+            ricerca:"",
+            page:"1",
             url: "https://api.themoviedb.org/3/search/movie?api_key=75ebf4a5bf0762d3887d0146d0d76336&query=",
-
+            films : "",
         }
     },
     methods:{
         getFilms(){
             console.log("ciao");
-            this.url = this.url + this.francesco;
+            this.films="";
+            // console.log("text ricerca : ",this.ricerca);
+            this.url = this.url + this.ricerca;
+            // console.log("url get : ",this.url);
             axios
                 .get(this.url)
                 .then(response =>{
                     console.log(response);
+                    console.log(response.data.total_pages);
+                    console.log(response.data.results.length);
+                    console.log(response.data.results[0]);
+                    this.films=response.data.results;
+                    // console.log(this.films.title);s
+                    // console.log("films :  ",this.films);
+                    
                 })
+            this.ricerca="";
+            this.url="https://api.themoviedb.org/3/search/movie?api_key=75ebf4a5bf0762d3887d0146d0d76336&query=";
         }
     }
 }
