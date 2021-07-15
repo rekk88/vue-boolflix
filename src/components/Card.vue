3132fd3e462be7d76d4159607b35c3aa2b6b1d79
maicol <maicol.matteoli@gmail.com>
2021-07-15 12:29:00 +0200

    add : Cast members in descrizione

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -8,9 +8,18 @@
             <div v-if="originalTitle != title">titolo originale : {{originalTitle}}</div>
             <div> <img :src="require('@/assets/images/'+this.originalLanguage+'.png')" alt="immagine"></div> 
             <div>
-              voto : {{calcoloVoto(voto)}} 
+              <!-- voto : {{calcoloVoto(voto)}}  -->
               <i v-for="index in calcoloVoto(voto)" :key="index" class="fas fa-star"></i>
               <i v-for="index in (5-calcoloVoto(voto))" :key="index" class="far fa-star"></i>
+            </div>
+            <div>
+              {{idProva}}  <!--debug-->
+              {{descrizione}}
+            </div>
+            <div v-if="cast.length > 0"><h4 class="mt-2">Cast</h4></div>
+            <div class="cast" v-for="(p,index) in cast.slice(0 , 5)" :key="index">
+
+              {{p.name}}
             </div>
           </div>
     </div>    
@@ -19,29 +28,46 @@
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
     name:'Cards',
     data() {
       return {
+        idProva : this.id,
+        cast : "",
       }
     },
     props:{
-      originalLanguage:String,
+      id : Number, 
+      originalLanguage: String,
       title : String,
       originalTitle: String,
+      descrizione : String,
       voto: Number,
       popularity:Number,
       type: String,
-      locandina:String,
-      path:String
+      locandina: String,
+      path: String
     },
     methods:{
       calcoloVoto(voto){
         // console.log("voto :" , voto);
         return Math.round(voto / 2)
-      }
+      },
     },
+
+    mounted(){
+      let urlCast = "https://api.themoviedb.org/3/"+this.type+"/"+this.id+"/credits?api_key=75ebf4a5bf0762d3887d0146d0d76336"
+      console.log(urlCast);
+      axios
+        .get(urlCast)
+        .then(response =>{
+          console.log(response.data.cast);
+          this.cast=response.data.cast;
+        })
+
+    }
     
 }
 
@@ -57,18 +83,26 @@ export default {
   top: 0;
   width:100%;
   height: 100%;
+  overflow-y: auto;
   background-color: black;
   opacity: 0;
   transition: linear .2s;
+
 }
 
 .card{
   height: 100%;
-  &:hover{
+  &:hover{ //.card on hover 
     .card-body{
       visibility:visible;
       opacity: 0.8;
+      
     }
+  }
+  .card-body{
+      .fa-star{
+          color: #ffc107;
+        }
   }
   .card-img-top{
     height: 100%;
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,25 +3,29 @@
     delle propietà è diverso , devo prima andare a scomporre l'oggetto e passare
     le propietà tramite props e NON l'oggetto per intero  -->
   <div class="main_wrapper container py-3">
-        <div class="row row-cols-3">
-            <Card v-for="element in film" :key="element.id" 
+        <div class="row row-cols-3 gy-2">
+            <Card v-for="(element,index) in film" :key="index" 
+                :id="element.id" 
                 :locandina="'https://image.tmdb.org/t/p/w342'+element.poster_path"
                 :path=element.poster_path
                 :title="element.title"
                 :originalTitle="element.original_title"
-                :type="'Film'"
+                :descrizione="element.overview"
+                :type="'movie'"
                 :originalLanguage="element.original_language"
                 :voto="element.vote_average"
                 :popularity="element.popularity"
             /> 
-            <Card v-for="element2 in series" :key="element2.id" 
+            <Card v-for="(element2,index2) in series" :key="index2"
+                :id="element2.id" 
                 :locandina="'https://image.tmdb.org/t/p/w342'+element2.poster_path"
                 :path=element2.poster_path
-                :title="element2.original_name"
+                :title="element2.name"
                 :originalTitle="element2.original_name"
-                :type="'Serie'"
+                :descrizione="element2.overview"
+                :type="'tv'"
                 :originalLanguage="element2.original_language"
-                :voto="element2.vote_average" 
+                :voto="element2.vote_average"
                 :popularity="element2.popularity"
             />
         </div>
@@ -37,7 +41,37 @@ export default {
         Card
     },
       //      :originalTitle="element.original_title"
-    props:["film","series"] //le prop in arrivo da app.vue 
+    props:["film","series"], //le prop in arrivo da app.vue 
+    data() {
+        return {
+            tvGenres : this.series,
+            filmIDs : this.film.id,
+            IDsSerie : [],
+            IDsFilm : []
+
+        }
+    },
+    methods: {
+        getGenreIDs(){
+            console.log(this.tvGenres);
+            console.log(this.series[0].genre_ids);
+        }
+    },
+    // updated(){
+    //     //raccatto tutti gli id 
+    //     console.log(this.tvGenres);
+    //     this.series.forEach(element => {
+    //         this.IDsSerie.push(element.id)
+    //     });
+    //     this.film.forEach(element => {
+    //         if(!this.IDsFilm.includes(element.id)){
+    //             this.IDsFilm.push(element.id)
+    //         }
+    //     });
+    //     console.log(this.IDsSerie);
+    //     console.log(this.IDsFilm);
+
+    // }
 }
 </script>
 
